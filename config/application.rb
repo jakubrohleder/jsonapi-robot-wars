73102b70a1a5a683c87d9504d0674070f048348c
Jakub Rohleder <jakubrohleder@gmail.com>
2015-09-01 20:39:33 +0200

    Apprename, config fixes

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AngularJsonapiBack
+module JsonapiRobotWars
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-AngularJsonapiBack::Application.config.secret_token = '03cc11da031420ece51ff848a0dbc47c5e04972a24861311b090c7ea15edfec607ab271ccf2ee851a6bcf1f21ae3860e9e18a840b7a511ed1bd8f4d31226bf02'
+JsonapiRobotWars::Application.config.secret_token = ENV['SECRET_KEY_BASE'] || '03cc11da031420ece51ff848a0dbc47c5e04972a24861311b090c7ea15edfec607ab271ccf2ee851a6bcf1f21ae3860e9e18a840b7a511ed1bd8f4d31226bf02'
