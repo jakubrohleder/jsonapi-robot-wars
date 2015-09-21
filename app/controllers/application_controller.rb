3a01d2b7d468e2aae1d94c95c1691569e9315564
Jakub Rohleder <jakubrohleder@gmail.com>
2015-09-21 13:42:06 +0200

    Removed delay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,15 @@ class ApplicationController < JSONAPI::ResourceController
   before_action :delay
 
   def delay
-    sleep(1.5)
+    # sleep(3)
   end
 
   rescue_from StandardError do |exception|
-    render :json => {
+    render json: {
       status: 500,
       title: 'Internal server error'
-    }, :status => 500
+    }, status: 500
 
     throw exception
   end
-
 end
